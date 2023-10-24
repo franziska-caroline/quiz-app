edf26e3f219e97d9434375f1ca61fbdb58185b95
Franzi <143591286+franziska-caroline@users.noreply.github.com>
2023-10-24 17:52:49 +0000

    add js to bookmarkbutton

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
+
 function toggleBookmark() {
-    const image = document.getElementById('bookmark-image');
-    const currentImageSrc = image.getAttribute('src');
-  
-    if (currentImageSrc === 'images/bookmark-icon-unmarked.png') {
-      image.setAttribute('src', 'images/bookmark-icon-marked.png');
-    } else {
-      image.setAttribute('src', 'images/bookmark-icon-unmarked.png');
-    }
+  console.log("hallo");
+  const button = document.querySelector('[data-js="button-bookmark"]');
+  const image = button.querySelector('[data-js="bookmark-icon-unmarked"]');
+
+  if (image.src.endsWith('bookmark-icon-unmarked.png')) {
+    image.src = 'images/bookmark-icon-marked.png';
+  } else {
+    image.src = 'images/bookmark-icon-unmarked.png';
   }
+}
